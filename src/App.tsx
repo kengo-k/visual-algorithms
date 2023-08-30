165f6803af114ece47c8227fb11708bc3ed0a9d0
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-30 13:18:35 +0900

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { animated, config, useSpring } from "react-spring";
+interface BoxProps {
+  value: number;
+}
+
+const Box: React.FC<BoxProps> = ({ value }) => {
+  return (
+    <div className="w-16 h-16 border border-black m-4 flex items-center justify-center bg-gradient-to-r from-blue-500 to-teal-500 text-white shadow-lg">
+      {value}
+    </div>
+  );
+};
+
+interface SimpleProps {
+  values: number[];
+}
+
+const Simple: React.FC<SimpleProps> = ({ values }) => {
+  return (
+    <div className="flex">
+      {values.map((value, index) => (
+        <Box key={index} value={value} />
+      ))}
+    </div>
+  );
+};
 
 function App() {
-  const props = useSpring({
-    to: { transform: "translateX(100px)" },
-    from: { transform: "translateX(0px)" },
-    config: config.wobbly,
-  });
-
-  return <animated.div style={props}>I will move</animated.div>;
+  return <Simple values={[1, 2, 3, 4, 5]} />;
 }
 
 export default App;
