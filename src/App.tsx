506b7f33b24064aa45d269eaeb8cff20aaa33888
Kengo.K <31839234+kengo-k@users.noreply.github.com>
2023-08-30 11:44:48 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
+  return <div>Hello, React!!</div>;
+};
+
+export default App;
