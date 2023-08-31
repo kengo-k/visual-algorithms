1b7aa729885e07849c7636953af49cd5b9b00908
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-31 23:16:27 +0900

    add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
+import { animated, useSpring } from "react-spring";
+import { create } from "zustand";
+
+interface Location {
+  x: number;
+  y: number;
+}
+
+interface State {
+  values: number[];
+  locations: {
+    [value: string]: Location;
+  };
+  init: (values: number[]) => void;
+}
+
+const useStore = create<State>((set) => ({
+  values: [],
+  locations: {},
+  init: (values: number[]) => {
+    set({ values });
+  },
+}));
+
 interface BoxProps {
   value: number;
 }
 
 const Box: React.FC<BoxProps> = ({ value }) => {
+  const styles = useSpring({ transform: "translate3d(0, 0, 0)" });
   return (
-    <div className="w-16 h-16 border border-black m-4 flex items-center justify-center bg-gradient-to-r from-blue-500 to-teal-500 text-white shadow-lg">
-      {value}
-    </div>
+    <animated.div style={styles}>
+      <div className="w-16 h-16 border border-black m-4 flex items-center justify-center bg-gradient-to-r from-blue-500 to-teal-500 text-white shadow-lg">
+        {value}
+      </div>
+    </animated.div>
   );
 };
 
@@ -25,7 +53,11 @@ const Simple: React.FC<SimpleProps> = ({ values }) => {
 };
 
 function App() {
-  return <Simple values={[1, 2, 3, 4, 5]} />;
+  const { values, init } = useStore();
+  useEffect(() => {
+    init([3, 7, 1, 5, 2, 9, 8, 4, 6]);
+  }, [init]);
+  return <Simple values={values} />;
 }
 
 export default App;
