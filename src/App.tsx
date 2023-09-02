e6595cd4668b9315bb126eb8e8fce6dab5a28ba3
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-09-02 13:22:11 +0900

    exec insertion sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,30 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { create } from "zustand";
-
-function* initRun() {}
-
-interface Element {
-  value: number;
-  x: number;
-  y: number;
-}
-
-interface Location {
-  x: number;
-  y: number;
-}
 
 interface State {
   values: number[];
   locations: {
-    [value: string]: Location;
+    [value: string]: { x: number; y: number };
   };
-  init: (values: number[]) => void;
-  run: Generator<undefined, void, unknown>;
-  setRun: (run: Generator<undefined, void, unknown>) => void;
+  index: number;
+  init: (values: number[], index: number) => void;
   move: (value: number, x: number, y: number) => void;
 }
 
 const useStore = create<State>((set) => ({
   values: [],
   locations: {},
-  init: (values: number[]) => {
-    set({ values });
+  index: 0,
+  init: (values: number[], index: number) => {
     const locations: State["locations"] = {};
     for (const v of values) {
       locations[v] = { x: 0, y: 0 };
     }
-    set({ locations });
+    set({ values, locations, index });
+  },
+  update: (values: number[], index: number) => {
+    set({ values, index });
   },
   move: (value: number, x: number, y: number) => {
     set((state) => {
@@ -44,27 +33,20 @@ const useStore = create<State>((set) => ({
       return { ...state, locations };
     });
   },
-  run: initRun(),
-  setRun: (run: Generator<undefined, void, unknown>) => {
-    set({ run });
-  },
 }));
-
-interface BoxProps {
-  value: number;
-}
 
 const size = 20;
 
-const Box: React.FC<BoxProps> = ({ value }) => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+const Box: React.FC<{ value: number }> = ({ value }) => {
+  //const [position, setPosition] = useState({ x: 0, y: 0 });
   const { locations } = useStore();
   const loc = locations[value];
-  useEffect(() => {
-    setPosition((prev) => ({ x: prev.x + loc.x, y: prev.y + loc.y }));
-  }, [loc]);
+  console.log("render box:", value, loc);
+  // useEffect(() => {
+  //   setPosition((prev) => ({ x: prev.x + loc.x, y: prev.y + loc.y }));
+  // }, [loc]);
   return (
-    <motion.div animate={position}>
+    <motion.div animate={loc}>
       <div
         className={`w-${size} h-${size} border-dashed border-2 border-sky-200 rounded-md bg-sky-50 flex items-center justify-center text-2xl font-bold text-blue-800`}
       >
@@ -74,51 +56,60 @@ const Box: React.FC<BoxProps> = ({ value }) => {
   );
 };
 
-function* myMove(
+const sleep = () => {
+  return new Promise((resolve) => setTimeout(resolve, 300));
+};
+
+async function insert(
   move: (value: number, x: number, y: number) => void,
   values: number[],
-  fromValue: number,
-  toValue: number
+  fromIndex: number,
+  toIndex: number
 ) {
-  console.log("fromValue", fromValue, "toValue", toValue);
-  const from = values.findIndex((v) => v === fromValue) + 1;
-  const to = values.findIndex((v) => v === toValue) + 1;
-  console.log("from", from, "to", to);
-  move(fromValue, 0, size * 4);
-  yield;
-  for (let i = from; i < to; i++) {
-    move(values[i], -size * 4, 0);
+  let from = values.findIndex((v) => v === values[fromIndex]);
+  let to = values.findIndex((v) => v === values[toIndex]);
+  move(values[fromIndex], 0, size * 4);
+  await sleep();
+  if (from > to) {
+    for (let i = to; i < from; i++) {
+      move(values[i], size * 4, 0);
+    }
+    await sleep();
+    move(values[fromIndex], -(from - to) * size * 4, -size * 4);
+  } else {
+    for (let i = from; i < to; i++) {
+      move(values[i], -size * 4, 0);
+    }
+    await sleep();
+    move(values[fromIndex], (to - from) * size * 4, -size * 4);
   }
-  yield;
-  move(fromValue, (to - from) * size * 4, -size * 4);
-  yield;
+  await sleep();
 }
 
 const Simple: React.FC = () => {
-  const { move, values, setRun, run } = useStore();
-  const onClick = () => {
-    run.next();
+  const { move, values, index, init } = useStore();
+  const onClick = async () => {
+    const newValues = [...values];
+    const value = newValues[index];
+    let j = index - 1;
+    while (j >= 0 && newValues[j] > value) {
+      newValues[j + 1] = newValues[j];
+      j--;
+    }
+    newValues[j + 1] = value;
+    await insert(move, values, index, j + 1);
+    init(newValues, index + 1);
   };
 
-  useEffect(() => {
-    function* generator() {
-      console.log("values", values);
-      const g1 = myMove(move, values, 1, 9);
-      const g2 = myMove(move, values, 7, 4);
-      yield* g1;
-      yield* g2;
-    }
-    setRun(generator());
-  }, [values, move, setRun]);
-
+  console.log("render:", values);
   return (
     <>
       <div className="flex">
         {values.map((value) => (
           <Box key={value} value={value} />
         ))}
+        <button onClick={onClick}>click</button>
       </div>
-      <button onClick={onClick}>click</button>
     </>
   );
 };
@@ -126,7 +117,7 @@ const Simple: React.FC = () => {
 function App() {
   const { init } = useStore();
   useEffect(() => {
-    init([3, 7, 1, 5, 2, 9, 8, 4, 6]);
+    init([7, 3, 1, 5, 2, 9, 8, 4, 6], 1);
   }, [init]);
   return <Simple />;
 }
